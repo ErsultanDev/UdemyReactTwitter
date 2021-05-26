fc587cba486d8feff04bd5412bb7a198add66c7a
ErsultanDev <80091465+ErsultanDev@users.noreply.github.com>
2021-05-26 18:20:05 +0600

    Add files via upload

---
+++ precache-manifest.d9d43364bfbf20b5604955be76cdbdc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d0c15cb58bb6bbb82fa",
+    "url": "/static/css/main.1b267526.chunk.css"
+  },
+  {
+    "revision": "6d0c15cb58bb6bbb82fa",
+    "url": "/static/js/main.6d0c15cb.chunk.js"
+  },
+  {
+    "revision": "77e068b90b4ec8588eac",
+    "url": "/static/css/1.f70f4fd6.chunk.css"
+  },
+  {
+    "revision": "77e068b90b4ec8588eac",
+    "url": "/static/js/1.77e068b9.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "25f49ac7954a5b567d2b6ac2c15f7b33",
+    "url": "/index.html"
+  }
+];
